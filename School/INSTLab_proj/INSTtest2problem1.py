2beba53968c364b505eb945c33acbb2301f09c68
Justin <dyer1300@gmail.com>
2022-04-20 08:19:12 -0500

    INST

--- School/INSTLab_proj/INSTtest2problem1.py
+++ School/INSTLab_proj/INSTtest2problem1.py
@@ -4,26 +4,32 @@ Created on Tue Apr 19 12:53:30 2022
 
 @author: Justin
 """
+##Instrumentation Assesment 2 Problem 1
 
+##Import needed modules
 import numpy as np
 import matplotlib.pyplot as plt
 
+##Givens
 Vin = 1.28
 Vup = 5
 Vlow = 0
 bits = np.array([4,8,16,32])
+#Solution calculated by hand
 error_hand = np.array([2.34375,0.703125,0.0015625,0])
 
+#Print Givens
 print('Input voltage = ', Vin)
 print('Voltage range = ', Vlow,'-',Vup,'V')
 
+#Take input voltage and bits and convert to digital
 digitalout = []
 for i in [4,8,16,32]:
     out = np.trunc((Vin-Vlow)/((Vup-Vlow)/(2**i)))
     digitalout.append(out)
     print('Digital Output of', i, 'bit: ', out)
     
-
+#Take digital signal back to voltage
 count = 0
 Vreversed = []
 for i in [4,8,16,32]:
@@ -34,6 +40,7 @@ for i in [4,8,16,32]:
     Vreversed.append(Vrev)
     print('Digital signal converted to voltage of',i,'bit: ', Vrev, 'V')
 
+#compute error
 error = []
 count = 0
 for i in Vreversed:
@@ -42,7 +49,7 @@ for i in Vreversed:
     print('Error of ',bits[count], 'bit: ', e,'%')
     count = count+1
 
-
+#Plot
 plt.plot(bits,error_hand,'r-', label= 'Hand Computed')
 plt.plot(bits,error,'b-',label= 'Python Computed')
 plt.xlabel('N (number of bits)')
---
+++ School/INSTLab_proj/INSTtest2problem2.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Apr 19 14:06:17 2022
+
+@author: Justin
+"""
+
+##Instrumentation Assesment 2 Problem 2
